e1d3ba793140648b35dc7e2e3d0095d6bab713f6
lapin <lapin@kontur.ru>
2017-10-12 08:32:26 +0400

    rename environment

--- Vostok.Contrails.Api/Startup.cs
+++ Vostok.Contrails.Api/Startup.cs
@@ -74,7 +74,7 @@ namespace Vostok.Contrails.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.Equals("dev", StringComparison.OrdinalIgnoreCase))
             {
                 app.UseDeveloperExceptionPage();
             }
